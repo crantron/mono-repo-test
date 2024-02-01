58efe6d4c82db2517aaaec2eb4f0f56e4add74d7
Chris Anderson <chrisanderson@rightpoint.com>
2024-02-01 06:00:48 -0800

    prepare release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symplify/monorepo-builder' => array(
+            'pretty_version' => '11.2.20',
+            'version' => '11.2.20.0',
+            'reference' => '444efe08701776cb1a03b9f3600e715697519aa6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symplify/monorepo-builder',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
