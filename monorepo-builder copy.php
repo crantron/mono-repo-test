ce5102a5c40c56c1d8c6963314aeb03b41d6dcb3
Chris Anderson <chrisanderson@rightpoint.com>
2024-02-27 17:47:27 -0800

    prepare release

--- monorepo-builder copy.php
+++ monorepo-builder copy.php
@@ -21,7 +21,7 @@ return static function (MBConfig $mbConfig): void {
         __DIR__ . '/global/src',
     ]);
     $mbConfig->defaultBranch('main');;
-    $mbConfig->packageAliasFormat('<major>.<minor>');
+    $mbConfig->packageAliasFormat('<major>.<minor>.x-dev');
     $mbConfig->workers([
         UpdateReplaceReleaseWorker::class,
         SetCurrentMutualDependenciesReleaseWorker::class,
