2fcaaeb2cc50432037bd9ea4cc7bca9417ca96c9
Chris Anderson <chrisanderson@RP-ChrisAnderson.home>
2024-09-04 15:27:14 -0700

    prepare release

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,17 +3,30 @@
 declare(strict_types=1);
 
 use Symplify\MonorepoBuilder\Config\MBConfig;
-use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $mbConfig): void {
     $mbConfig->packageDirectories([__DIR__ . '/packages']);
     $mbConfig->packageAliasFormat('<major>.<minor>.x-dev');
-//    $mbConfig->dataToAppend([
-//        ComposerJsonSection::REPOSITORIES => [
-//            'composer' => [
-//                'type' => 'composer',
-//                'url' => 'https://repo.packagist.com/crantron/'
-//            ]
-//        ]
-//    ]);
+
+    //default release workers, provided by monorepo library
+    //see: vendor/symplify/monorepo-builder/packages/Release/ReleaseWorker/*
+    $workers = [
+        UpdateReplaceReleaseWorker::class,
+        SetCurrentMutualDependenciesReleaseWorker::class,
+        AddTagToChangelogReleaseWorker::class,
+        TagVersionReleaseWorker::class,
+        PushTagReleaseWorker::class,
+        SetNextMutualDependenciesReleaseWorker::class,
+        UpdateBranchAliasReleaseWorker::class,
+        PushNextDevReleaseWorker::class
+    ];
+    $mbConfig->workers($workers);
 };
