46aee9c973c4fe945b04930910ca052665ba5ece
Chris Anderson <chrisanderson@RP-ChrisAnderson.home>
2024-09-08 08:34:11 -0700

    prepare release

--- workers/src/UpdatePackageVersion.php
+++ workers/src/UpdatePackageVersion.php
@@ -6,51 +6,44 @@ use PharIo\Version\Version;
 use Symplify\MonorepoBuilder\DevMasterAliasUpdater;
 use Symplify\MonorepoBuilder\FileSystem\ComposerJsonProvider;
 use Symplify\MonorepoBuilder\Utils\VersionUtils;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
 
 final class UpdatePackageVersion implements ReleaseWorkerInterface
 {
-    private SymfonyStyle $symfonyStyle;
-
     public function __construct(
         private DevMasterAliasUpdater $devMasterAliasUpdater,
         private ComposerJsonProvider $composerJsonProvider,
         private VersionUtils $versionUtils,
-    ) {
-//        $input = new Symfony\Component\Console\Input\ArgvInput();
-//        $output = new Symfony\Component\Console\Output\ConsoleOutput();
-//        $this->symfonyStyle = new SymfonyStyle($input, $output);
-    }
+    ) { }
 
     public function work(Version $version): void
     {
-//        $composerFiles = $this->getComposerJsonFiles();
-//
-//        foreach ($composerFiles as $composerJsonFile) {
-//            if (!file_exists($composerJsonFile)) {
-//                $this->symfonyStyle->error(sprintf('File not found: %s', $composerJsonFile));
-//                continue;
-//            }
-//
-//            $composerJsonContent = json_decode(file_get_contents($composerJsonFile), true);
-//
-//            if (json_last_error() !== JSON_ERROR_NONE) {
-//                $this->symfonyStyle->error(sprintf('Invalid JSON in %s', $composerJsonFile));
-//                continue;
-//            }
-//
-//            $composerJsonContent['version'] = $version->getVersionString();
-//
-//            $newContent = json_encode($composerJsonContent, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-//
-//            try {
-//                file_put_contents($composerJsonFile, $newContent);
-//                $this->symfonyStyle->success(sprintf('Updated version to "%s" in %s', $version->getVersionString(), $composerJsonFile));
-//            } catch (\Exception $exception) {
-//                $this->symfonyStyle->error(sprintf('Failed to update %s due to an error: %s', $composerJsonFile, $exception->getMessage()));
-//            }
-//        }
+        $composerFiles = $this->getComposerJsonFiles();
+
+        foreach ($composerFiles as $composerJsonFile) {
+            if (!file_exists($composerJsonFile)) {
+                echo sprintf('File not found: %s', $composerJsonFile) . PHP_EOL;
+                continue;
+            }
+
+            $composerJsonContent = json_decode(file_get_contents($composerJsonFile), true);
+
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                echo sprintf('Invalid JSON in %s', $composerJsonFile) . PHP_EOL;
+                continue;
+            }
+
+            $composerJsonContent['version'] = $version->getVersionString();
+
+            $newContent = json_encode($composerJsonContent, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+            try {
+                file_put_contents($composerJsonFile, $newContent);
+                echo sprintf('Updated version to "%s" in %s', $version->getVersionString(), $composerJsonFile) . PHP_EOL;
+            } catch (\Exception $exception) {
+                echo sprintf('Failed to update %s due to an error: %s', $composerJsonFile, $exception->getMessage()) . PHP_EOL;
+            }
+        }
     }
 
     public function getDescription(Version $version): string
